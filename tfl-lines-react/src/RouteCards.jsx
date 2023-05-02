c28f5636cc56b02c61c0b051fd3314e1d8b2ff0e
Lorena <lorenacapraru@gmail.com>
2023-05-02 15:51:01 +0100

    Update RouteCards.jsx

--- tfl-lines-react/src/RouteCards.jsx
+++ tfl-lines-react/src/RouteCards.jsx
@@ -2,20 +2,26 @@ import React from "react";
 
 export default function RouteCards(props) {
   return (
-    <div className="cardsContainer">
-      <div className="card">
-        <p className="descriptionLane">START OF LINE</p>
-        <p className="routeName">
-          {props.route.routeSections.map((el) => el.originationName)[0]}
-        </p>
-      </div>
-      <span>➔</span>
-      <div className="card">
-        <p className="descriptionLane">END OF LINE</p>
-        <p className="routeName">
-          {props.route.routeSections.map((el) => el.destinationName)[0]}
-        </p>
-      </div>
+    <div>
+      {props.route.routeSections && props.route.routeSections.length > 0 ? (
+        <div className="cardsContainer">
+          <div className="card">
+            <p className="descriptionLane">START OF LINE</p>
+            <p className="routeName">
+              {props.route.routeSections.map((el) => el.originationName)[0]}
+            </p>
+          </div>
+          <span>➔</span>
+          <div className="card">
+            <p className="descriptionLane">END OF LINE</p>
+            <p className="routeName">
+              {props.route.routeSections.map((el) => el.destinationName)[0]}
+            </p>
+          </div>
+        </div>
+      ) : (
+        <span>no routes available</span>
+      )}
     </div>
   );
 }
